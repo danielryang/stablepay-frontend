411a701348d03d50853d47d7d9090eacf8469d56
Principle <principle105@gmail.com>
2025-11-22 16:45:52 -0600

    removed unnecessary boilerplate and formatted code

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,15 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+// Minimal color constants for React Navigation tab bar
+// (Tab bars require color values, not Tailwind classes)
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+    light: {
+        tint: tintColorLight,
+        text: "#000",
+    },
+    dark: {
+        tint: tintColorDark,
+        text: "#fff",
+    },
 };
